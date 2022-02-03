e1795e0fc91234e88f8058f08045cf78fc7aab75
Jonah <jonah.watts@gmail.com>
2022-02-03 14:20:27 -0700

    Sarting Changes

--- command.sh
+++ command.sh
@@ -1,2 +1,34 @@
-STR = "hello world"
-echo STR
+#!/usr/bin/bash
+
+echo "enter your favorite number"
+read firstnumber
+echo "enter your second favorite number"
+read secondnumber
+
+if [ $firstnumber -eq 0 ] && [ $secondnumber -eq 0 ]; then 
+    echo "num1 and num2 are zero"
+
+elif [ $firstnumber -eq $secondnumber ]; then
+    echo "they are the same number"
+
+elif [ $firstnumber -gt $secondnumber ]; then
+    echo "first number is greater"
+
+else
+    echo "second is greater than first"
+
+fi
+
+
+echo "second test"
+
+if [ -z "$1" ]; then
+    echo usage: $0 directory
+    exit
+fi
+
+TEMP = $1
+echo "you entered $0 and $1"
+ls -l
+
+man man
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Bash Script Plan/Psudocode
+
+---
+
+## Game Plan:
+
+1. Get the numbner of command line arguments to loop around teh code multiple times
+  
+2. Loop and find the amount of different pages for the man entry
+  
+3. Output the code to a pdf file, and also show terminal that it has executed
+  
+4. Make cool text
