1b442a5f8f88d03198c0a34883c014fb1588819c
Jonah <jonah.watts@gmail.com>
2022-02-03 14:46:49 -0700

    Finish the main loop

--- mans.sh
+++ mans.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/bash
 
+# Loop through every argument
+for i in $@; do
 
+    # Check every page
+    echo the command line argument was $i
+    for j in {1..9}; do
+        man $i $j > "$i($j).txt"
+    done
+done
 
-echo 
