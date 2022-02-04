b18ab2697d5ec44d816f8465f668716d74176968
Jonah <jonah.watts@gmail.com>
2022-02-03 20:34:49 -0700

    mans.sh updated to give error message

--- mans.sh
+++ mans.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/bash
 
-#Jonah Watts
+# Jonah Watts
+# 200206
+# CMPT330
+
+# First see if the command is valid
+if $1 ; then
+    echo "USAGE $0 (item) [items]"
+fi
 
 
 # Loop through every argument
@@ -9,9 +16,13 @@ for i in $@; do
     # Check every page
     echo -n $i
     for j in {1..9}; do
+
+        # If there is a manual entry in this section, create a pdf
         if  man $j $i &> /dev/null ; then
+
             man -t $j $i | ps2pdf - "$i($j).pdf"
             echo -n  " "$j
+
         fi
     done
     echo
